b2b999a38e4e1939f464fa8ad6a3ad89ebe12b7f
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-09-22 21:30:42 -0300

    style: adjust styles

--- resources/views/livewire/components/todo-item.blade.php
+++ resources/views/livewire/components/todo-item.blade.php
@@ -1,37 +1,43 @@
-<div wire:key='{{ $todo->id }}' class="bg-blue-50 p-4 rounded">
+<div wire:key='{{ $todo->id }}' class="p-4 rounded bg-blue-50">
     <div class="flex items-center gap-2">
         @if ($todo->completed)
             <input type="checkbox" wire:click='isCompleted({{ $todo->id }})' name="checkbox" id="checkbox" checked>
         @else
             <input type="checkbox" wire:click='isCompleted({{ $todo->id }})' name="checkbox" id="checkbox">
         @endif
-        <h2 class="text-blue-600 font-bold text-lg">{{ $todo->name }}</h2>
+
+
+        <h2 class="relative text-lg font-bold text-blue-600">{{ $todo->name }}</h2>
 
         @if ($editingTodoID == $todo->id)
-            <input type="text" placeholder="Todo.." class="" wire:model='editingTodoName'>
-        @else
-            @error('editingTodoName')
-                <span>{{ $message }}</span>
-            @enderror
+            <div class="absolute flex justify-between w-max">
+                <input type="text" placeholder="Todo.." class="px-2 mr-4" wire:model='editingTodoName'>
+                @if ($editingTodoID === $todo->id)
+                    <div class="flex gap-2">
+                        <button wire:click='update' class="px-3 py-1 text-white bg-green-600 rounded">Update</button>
+                        <button wire:click='cancelEdit' class="px-3 py-1 text-white bg-red-600 rounded">Cancel</button>
+                    </div>
+            </div>
         @endif
+    @else
+        @error('editingTodoName')
+            <span>{{ $message }}</span>
+        @enderror
+        @endif
+
+
 
     </div>
-    <p class="break-words text-zinc-800 py-4">{{ $todo->description }}</p>
-    <div class="flex justify-between items-center">
+    <p class="py-4 break-words text-zinc-800">{{ $todo->description }}</p>
+    <div class="flex items-center justify-between">
 
         <span class="text-gray-500">{{ $todo->created_at }}</span>
-
-        @if ($editingTodoID === $todo->id)
-            <button wire:click='update'>Update</button>
-            <button wire:click='cancelEdit'>Cancel</button>
-        @endif
-
         <div class="flex gap-2">
             <button wire:click='edit({{ $todo->id }})'>
-                <x-lucide-pencil class="text-blue-700 hover:text-blue-500 w-5 h-5 cursor-pointer" />
+                <x-lucide-pencil class="w-5 h-5 text-blue-700 cursor-pointer hover:text-blue-500" />
             </button>
             <button wire:click='delete({{ $todo->id }})'>
-                <x-lucide-trash class="text-red-500 hover:text-red-800 w-5 h-5 cursor-pointer" />
+                <x-lucide-trash class="w-5 h-5 text-red-500 cursor-pointer hover:text-red-800" />
             </button>
         </div>
     </div>
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,40 +1,41 @@
-<div>
+<div class="px-4">
     <form class="border-t-4 border-blue-700">
-        <div class="flex justify-between items-center py-4">
-            <h1 class="text-center my-4 font-medium text-2xl">Create New Todo</h1>
+        <div class="flex items-center justify-between py-4">
+            <h1 class="my-4 text-2xl font-medium text-center">Create New Todo</h1>
             @include('livewire.components.todo-search')
         </div>
 
 
-        <div class="flex flex-col gap-4 mx-auto">
+        <div class="flex flex-col gap-4 mx-auto md:flex-row">
 
             <input type="text" placeholder="Add New Todo" id="name" wire:model="name"
-                class="border p-2 outline-none">
+                class="p-2 border outline-none md:w-full">
             @error('name')
-                <span class="text-red-500 text-xs">{{ $message }}</span>
+                <span class="text-xs text-red-500">{{ $message }}</span>
             @enderror
 
-            <textarea placeholder="Description" id="description" wire:model="description" class="border p-2 outline-none"></textarea>
+            <textarea placeholder="Description" id="description" wire:model="description" class="p-2 border outline-none md:w-full"></textarea>
             @error('description')
-                <span class="text-red-500 text-xs">{{ $message }}</span>
+                <span class="text-xs text-red-500">{{ $message }}</span>
             @enderror
 
         </div>
+
         <button type="submit" wire:click.prevent="create"
-            class="bg-blue-700 text-blue-50 font-bold py-2 px-4 w-max rounded hover:bg-blue-800 mt-4">
+            class="px-4 py-2 mt-4 font-bold bg-blue-700 rounded text-blue-50 w-max hover:bg-blue-800">
             Create +
         </button>
         @if (session('sucess'))
-            <span class="text-sm text-green-600 font-semibold">{{ session('sucess') }}</span>
+            <span class="text-sm font-semibold text-green-600">{{ session('sucess') }}</span>
         @endif
     </form>
 
-    <section class="grid grid-cols-1 gap-4 mt-8">
+    <section class="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-2">
         @forelse ($todos as $todo)
             @include('livewire.components.todo-item')
         @empty
-            <span class="text-center font-medium text-blue-300 flex flex-col gap-2 items-center">
-                <x-lucide-book-open class="text-center text-blue-300 w-12 h-12" />
+            <span class="flex flex-col items-center gap-2 font-medium text-center text-blue-300">
+                <x-lucide-book-open class="w-12 h-12 text-center text-blue-300" />
                 None all registered...
             </span>
         @endforelse
